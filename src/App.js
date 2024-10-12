c752767fc5eeaef10e93cc4a29ce67a810465dd8
Abd Sarari <eng_abd@abds-air.mynet>
2024-10-12 17:01:39 +0300

    analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Head from './Pages/Head/index'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
+    <Analytics />
       <br />
       <Head />
     </>
